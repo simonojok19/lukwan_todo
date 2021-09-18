699d8627a3d701a553051ed6db5170bb3cba4253
simonojok19 <simonojok19@gmail.com>
2021-09-18 12:31:31 +0300

    finished priority item compoment

--- app/src/main/java/com/lukwan/todo/components/PrirorityItem.kt
+++ app/src/main/java/com/lukwan/todo/components/PrirorityItem.kt
@@ -10,21 +10,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.lukwan.todo.data.models.Priority
+import com.lukwan.todo.ui.theme.LARGE_PADDING
+import com.lukwan.todo.ui.theme.PRIORITY_INDICATOR_SIZE
 import com.lukwan.todo.ui.theme.Typography
 
 @Composable
 fun PriorityItem(priority: Priority) {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Canvas(modifier = Modifier.size(16.dp)) {
+        Canvas(modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)) {
             drawCircle(color = priority.color)
         }
         Text(
             text = priority.name,
             style = Typography.subtitle1,
             color = MaterialTheme.colors.onSurface,
-            modifier = Modifier.padding(start = 12.dp)
+            modifier = Modifier.padding(start = LARGE_PADDING)
         )
     }
 }
