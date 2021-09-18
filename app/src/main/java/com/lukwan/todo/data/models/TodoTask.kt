0b74b68f043d651091a1c156d88aefbefb237ac4
simonojok19 <simonojok19@gmail.com>
2021-09-18 08:43:46 +0300

    finished entity class

--- app/src/main/java/com/lukwan/todo/data/models/TodoTask.kt
+++ app/src/main/java/com/lukwan/todo/data/models/TodoTask.kt
@@ -1,7 +1,14 @@
 package com.lukwan.todo.data.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.lukwan.todo.utils.Constants
 
-@Entity(tableName = )
-data class TodoTask {
-}
+@Entity(tableName = Constants.DATABASE_TABLE)
+data class TodoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
+)
