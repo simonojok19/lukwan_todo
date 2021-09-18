707e8179436936edccf1bc4c5e7a2192063a34a5
simonojok19 <simonojok19@gmail.com>
2021-09-18 10:38:08 +0300

    finished navigations

--- app/src/main/java/com/lukwan/todo/MainActivity.kt
+++ app/src/main/java/com/lukwan/todo/MainActivity.kt
@@ -3,16 +3,22 @@ package com.lukwan.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.lukwan.todo.navigation.SetupNavigation
 import com.lukwan.todo.ui.theme.TodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoTheme {
-
+                navHostController = rememberNavController();
+                SetupNavigation(navController = navHostController)
             }
         }
     }
